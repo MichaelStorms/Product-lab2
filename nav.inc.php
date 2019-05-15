b4a22cf7a8d1fd4fc174ec02eddf4ca78c300107
MichaelStorms <49272397+MichaelStorms@users.noreply.github.com>
2019-05-15 11:22:14 +0200

    nav

---
+++ nav.inc.php
@@ -0,0 +1,13 @@
+<nav class="navbar">
+    <a href="index.php" class="logo"><img src="spotless.png" alt="spotless logo"></a>
+    <a href="index.php"><img src="images/lightbulb.svg" alt="tips and tricks"></a>
+    <a href="index.php"><img src="images/global.svg" alt="global"></a>
+    <a href="mylist.php"><img src="task-complete.svg" alt="events"></a>
+
+    
+    <form action="" method="get">
+      <input type="text" name="search">
+    </form>
+    
+    
+</nav>
