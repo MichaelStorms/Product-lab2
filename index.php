69a6faa85cc0653cd09c11c0cdc8ca16a986e4e2
MiguelLamm <49272909+MiguelLamm@users.noreply.github.com>
2019-05-15 11:12:42 +0200

    basic html en css

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Spotless</title>
+</head>
+<body>
+<?php include 'nav.inc.php'; ?>
+<h1>Recent posts</h1>
+<?php foreach($post as $p): ?>
+
+
+<?php endforeach;?>
+</body>
+</html>
