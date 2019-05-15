f8dc0877732b4f1292d8bfc49afbcb71d1565233
MiguelLamm <49272909+MiguelLamm@users.noreply.github.com>
2019-05-15 11:54:32 +0200

    includes

---
+++ includes/nav.inc.php
@@ -0,0 +1,6 @@
+<nav class="navbar">
+    <a href="index.php" class="logo"><img src="spotless.png" alt="spotless logo"></a>
+    <a href="index.php"><img src="images/lightbulb.svg" alt="tips and tricks"></a>
+    <a href="index.php"><img src="images/global.svg" alt="global"></a>
+    <a href="mylist.php"><img src="task-complete.svg" alt="events"></a>
+</nav>
